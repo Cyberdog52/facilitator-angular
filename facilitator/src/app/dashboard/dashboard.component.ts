27f2878a9b01235ed1e483d7f241901871540f5f
Niklaus, Marius <marius.niklaus@zuehlke.com>
2021-10-13 14:31:56 +0200

    setup components and routing

---
+++ facilitator/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Member } from '../members/member';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  members: Member[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+  }
+
+  getMembers(): void {
+    this.dataService.getMembers().subscribe(members => this.members = members);
+  }
+}
---
+++ facilitator/src/app/members/mock-members.ts
@@ -0,0 +1,8 @@
+import { Member } from './member';
+
+export const MEMBERS: Member[] = [
+  { id: 1, surname: 'Dr.', name: 'Nice', superpower:'Blinding Light'},
+  { id: 2, surname: 'Dave', name: 'Boba', superpower:'Fat'},
+  { id: 3, surname: 'Who', name: 'That', superpower:'Ignorance'},
+  { id: 4, surname: 'Andy', name: 'Newton', superpower:'Knowledge'},
+];
