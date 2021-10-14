82bb2d15ed5d94aef3c801b431f92e4adabf430d
Niklaus, Marius <m.n.niklaus.97@gmail.com>
2021-10-14 16:14:41 +0200

    add routing for remaining detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
 import { GamesComponent } from './games/games.component';
+import { MeetingDetailComponent } from './meeting-detail/meeting-detail.component';
 import { MeetingsComponent } from './meetings/meetings.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
 import { MembersComponent } from './members/members.component';
+import { RoomDetailComponent } from './room-detail/room-detail.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { TopicDetailComponent } from './topic-detail/topic-detail.component';
 import { TopicsComponent } from './topics/topics.component';
 
 const routes: Routes = [
@@ -16,7 +20,11 @@ const routes: Routes = [
   { path: 'games', component: GamesComponent },
   { path: 'rooms', component: RoomsComponent },
   { path: 'topics', component: TopicsComponent },
-  { path: 'member/:id', component: MemberDetailComponent }
+  { path: 'meeting/:id', component: MeetingDetailComponent },
+  { path: 'member/:id', component: MemberDetailComponent },
+  { path: 'game/:id', component: GameDetailComponent },
+  { path: 'room/:id', component: RoomDetailComponent },
+  { path: 'topic/:id', component: TopicDetailComponent }
 ];
 
 @NgModule({
