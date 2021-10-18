e1a4210133b498d7aa892c024332cc9ceac432b7
gmoh <georg.mohr@zuehlke.com>
2021-10-18 11:31:43 +0200

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { MembersComponent } from './components/members/members.component';
-import { GamesComponent } from './components/games/games.component';
-import { MeetingsComponent } from './components/meetings/meetings.component';
-import { RoomsComponent } from './components/rooms/rooms.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
-import { TopicsComponent } from './components/topics/topics.component';
-import { RoomDetailComponent } from './components/rooms/room-detail/room-detail.component';
-import { TopicDetailComponent } from './components/topics/topic-detail/topic-detail.component';
-import { GameDetailComponent } from './components/games/game-detail/game-detail.component';
-import { MeetingDetailComponent } from './components/meetings/meeting-detail/meeting-detail.component';
-import { FormsModule } from '@angular/forms';
+import {AppComponent} from './app.component';
+import {MembersComponent} from './components/members/members.component';
+import {GamesComponent} from './components/games/games.component';
+import {MeetingsComponent} from './components/meetings/meetings.component';
+import {RoomsComponent} from './components/rooms/rooms.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {MemberDetailComponent} from './components/members/member-detail/member-detail.component';
+import {TopicsComponent} from './components/topics/topics.component';
+import {RoomDetailComponent} from './components/rooms/room-detail/room-detail.component';
+import {TopicDetailComponent} from './components/topics/topic-detail/topic-detail.component';
+import {GameDetailComponent} from './components/games/game-detail/game-detail.component';
+import {MeetingDetailComponent} from './components/meetings/meeting-detail/meeting-detail.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
 
 @NgModule({
   declarations: [
@@ -36,9 +38,12 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
